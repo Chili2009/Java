fc95b8b950a5f271b3e3c6ac75075ec1a93c9cce
Mia <mia.fernandezrannikko@metropolia,fi>
2022-11-11 14:31:16 +0200

    Java

--- Moduuli 1/teht1.js
+++ Moduuli 1/teht1.js
@@ -1,8 +1,8 @@
-'use strict';
+/*'use strict';
 
 let nimi = prompt("Minkä niminen olet?")
 document.querySelector("#kohde").innerHTML = 'Moi ' + nimi + '!'
-
+*/
 'use strict';
 
 let kohde = document.querySelector("#kohde");
---
+++ Moduuli 2/teht1.js
@@ -0,0 +1,13 @@
+let luvut = [];
+
+for (let i = 0; i < 5; i++)
+{
+    luvut[i]= parseInt(prompt("Anna luku"));
+}
+
+for (let i = 4; i >= 0; i--)
+{
+   console.log(luvut);
+}
+
+
