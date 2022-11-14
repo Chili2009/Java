180a3cc87a1c88ae2f5de87594e1f205470b0154
Mia <mia.fernandezrannikko@metropolia,fi>
2022-11-14 16:15:59 +0200

    Java

--- Moduuli 2/teht2.js
+++ Moduuli 2/teht2.js
@@ -1,9 +1,23 @@
+//Kysytään osanottajien määrä
 let nof_p = parseInt(prompt("How many participants?"));
 let participants = [];
 
+//luetaan osanottajien nimet
 for (let i = 0; i < nof_p; i++)
 {
     participants[i] = prompt("Name");
 }
 
-console.log(participants)
+//generoidaan HTML-öista osanottajien nimistä
+let html ='<ol>';
+for (let i = 0; i < nof_p; i++)
+{
+    html += '<li>';
+    html += participants[i];
+    html += '</li>';
+}
+html += '</ol>';
+
+//lisätään generoitu html sivulle
+let kohde = document.querySelector('#Kohde');
+kohde.innerHTML = html
